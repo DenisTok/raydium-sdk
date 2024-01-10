d8a6c77686f8381dcfa96dd3b51d30f9fd56ce58
DenisTok <7974784+DenisTok@users.noreply.github.com>
2024-01-10 15:28:56 +0400

    mainnet

--- src/base/lookupTableCache.ts
+++ src/base/lookupTableCache.ts
@@ -3,11 +3,11 @@ import { AddressLookupTableAccount, PublicKey } from '@solana/web3.js'
 import { CacheLTA } from '../common'
 
 export const LOOKUP_TABLE_CACHE: CacheLTA = {
-  'Cg21j6b5vAQ9xph1CJZdofhdFRoRAqXhP7K6rcx1tbnN': new AddressLookupTableAccount({
-    key: new PublicKey('Cg21j6b5vAQ9xph1CJZdofhdFRoRAqXhP7K6rcx1tbnN'),
+  '7LGeYqzxw6uxXDtnVqrjZgCeWkHf8G2mo6b5BAozRyb2': new AddressLookupTableAccount({
+    key: new PublicKey('7LGeYqzxw6uxXDtnVqrjZgCeWkHf8G2mo6b5BAozRyb2'),
     state: AddressLookupTableAccount.deserialize(
       Buffer.from(
-        'AQAAAP//////////1WI3DwAAAAAAAU8/WFFOvhIexdxEQUAkvKL32XrI5XkvcrKtZlnrQyYoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMGRm/lIRcy/+ytunLDm+e8jOW7xfcSayxDmzpAAAAABt324ddloZPZy+FGzut5rBy0he1fWzeROoz1hX7/AKkG3fbh7nWP3hhCXbzkbM3athr8TYO5DSf+vfko2KGL/AVKU1D4XciC1hSlVnJ4iilt3x6rq9CmBniISTL07vagBqfVFxksXFEhjMlMPUrxf1ja7gibof1E49vZigAAAAAGp9UXGMd0yShWY5hpHV62i164o5tLbVxzVVshAAAAAIyXJY9OJInxuz0QKRSODYMLWhOZ2v8QhASOe9jb6fhZC3BlsePRfEU4nVJ/awTDzVi4bHMaoP21SbbRvAP4KUY=',
+        'AQAAAP//////////5dlcDgAAAAAAAZSEalLG/Lh78sninM1CKGDr+iw7tXDI+FxeGje4ahPsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMGRm/lIRcy/+ytunLDm+e8jOW7xfcSayxDmzpAAAAABt324ddloZPZy+FGzut5rBy0he1fWzeROoz1hX7/AKkG3fbh7nWP3hhCXbzkbM3athr8TYO5DSf+vfko2KGL/AVKU1D4XciC1hSlVnJ4iilt3x6rq9CmBniISTL07vagBqfVFxksXFEhjMlMPUrxf1ja7gibof1E49vZigAAAAAGp9UXGMd0yShWY5hpHV62i164o5tLbVxzVVshAAAAAIyXJY9OJInxuz0QKRSODYMLWhOZ2v8QhASOe9jb6fhZC3BlsePRfEU4nVJ/awTDzVi4bHMaoP21SbbRvAP4KUYJDa7gkEeFDgD6nZXa1EjeVqBbL6tGCP/JiGcpqNpeAA==',
         'base64',
       ),
     ),
